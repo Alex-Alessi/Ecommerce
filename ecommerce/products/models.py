b423ddc0a78a78eabc1df54b4ad5d1781cc57544
Alex Alessi <alex.alessi246@gmail.com>
2026-02-19 16:47:55 +0100

    first commit

---
+++ ecommerce/products/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(unique=True, max_length=100)
+    description = models.TextField(blank=True)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(min_value=0)
