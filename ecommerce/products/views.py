69ab49dad61ca9d1765f50fb17a457977a9c40f3
Alex Alessi <alex.alessi246@gmail.com>
2026-02-24 16:05:59 +0100

    bozza protucts

--- ecommerce/products/views.py
+++ ecommerce/products/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Category, Product
 
 # Create your views here.
+
+def catalogo(request):
+    prodotti = Product.objects.filter(is_active=True).order_by('-created_at')
+
+    context={"prodotti": prodotti}
+
+    return render(request, 'products/catalogo.html', context)
